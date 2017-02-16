b4bea069f37c1152ccc1222837ddd78dda3cda5f
bangback3 <bangback3@gmail.com>
2017-02-16 14:31:49 +0900

    2017.02.16

---
+++ 2017.02.08/js/ms_event.js
@@ -0,0 +1,36 @@
+$(function(){ //콜백함수는 함수안에 인자가 있는것.ex)함수 안에 함수.
+
+    $('.open').click(function(){
+        bl();
+        var tg = '.'+$(this).attr('id')//attr는 대상 안에서 정해준대상을 뽑아온다
+        var w = $(tg).width()
+        var h = $(tg).height()
+        $(tg).show().css({
+            left:'50%',
+            top:function(){
+                return $(window).scrollTop() + 'px'
+            },
+            marginLeft:function(){
+                return '-'+(w / 2)+"px"//return은 그 대상으로 가령 식이라면
+                                      //그 식대로 변환해 준는것
+            }
+        })
+    })
+
+    var close = function(){
+
+        $('.pop').hide();
+        $('.bl').fadeOut(function(){
+            $(this).remove()
+        })
+    }
+
+    $('body').on('click','.bl',close)
+    $('.close').click(close)
+
+    function bl(){
+        $('body').prepend('<div class="bl"></div>')
+    }
+
+
+})
