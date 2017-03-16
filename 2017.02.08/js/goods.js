53f6d623afeab0e0935babd5204f44650eb91194
bangback3 <bangback3@gmail.com>
2017-03-16 18:42:08 +0900

    2017.03.16

--- 2017.02.08/js/goods.js
+++ 2017.02.08/js/goods.js
@@ -1,10 +1,31 @@
-$(document).ready(function(){
-    $('.tab_area .sd div').hide();
-    $('.tab_area .sd div').first().show();
+// $(document).ready(function(){
+//     $('.tab_area .sd div').hide();
+//     $('.tab_area .sd div').first().show();
+//
+//      $('ul.tab_list a').click(function () {
+//         var idx = $(this).index();
+//
+//         $('.sd div').eq(idx).show().siblings().hide();
+//         console.log('@@');
+//     });
+//     // $('.tab_area').on('click','.tab_list a', function(e){
+//     //     var $parent = $(this).parent();
+//     //     var _index = $parent.index();
+//     //     var $content = $(e.delegateTarget).find('.sd div').eq(_index);
+//     //     e.preventDefault();
+//     //
+//     //     $parent.add($content).addClass('active').siblings().removeClass('active');
+//     // }).find('.tab_list a').eq(0).trigger('click');
+// });
+$(function(){
 
-     $('ul.tab_list li').click(function () {
-        var idx = $(this).index();
+    var idx = 0 ;
 
-        $('.sd div').eq(idx).show().siblings().hide();
-    });
-});
+    $('.sd > div').eq(0).css('display','block')
+
+    $('.tab_list li').click(function(){
+        idx = $(this).index();
+        $('.sd > div').eq(idx).css('display','block').siblings().css('display','none')
+    })
+
+})
