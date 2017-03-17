285fc18e500831cf8d5abc6a6ade4813edc14129
bangback3 <bangback3@gmail.com>
2017-03-17 18:29:13 +0900

    2017.03.17

--- 2017.02.08/js/goods.js
+++ 2017.02.08/js/goods.js
@@ -25,7 +25,46 @@ $(function(){
 
     $('.tab_list li').click(function(){
         idx = $(this).index();
+        $('.sd > div').eq(idx).css('display','block').siblings().css('display','none');
+
+        // $('#list'+idx).css({position:'absolute',top:'-150px'});
+
+    });
+
+    /*$('#list'+idx).mouseover(function(){
+        $(this).css({position:'absolute',top:'-150px'});
+    });*/
+    // $('#list'+idx).mouseout(function(){
+    //     $(this).eq(idx).css({position:'absolute',top:'0px'});
+    // });
+
+    /*$('.tab_list li').mouseleave(function(){
+        idx = $(this).index();
+        $('#list'+idx).css({top:'0px'});
+    }*/
+    /*('.tab_list li').click(function(){
+        idx = $(this).index();
         $('.sd > div').eq(idx).css('display','block').siblings().css('display','none')
-    })
+
+    })*/
+
+
+    //var list= $('.list');
+
+    // list.bind({
+    //     mouseover:function(){
+    //         var tg = $(this);
+    //         tg.removeClass().addClass('list over');
+    //     },
+    //     mouseout:function(){
+    //         var tg = $(this);
+    //         tg.removeClass().addClass('list out');
+    //     },
+    //     click:function(){
+    //         var tg = $(this);
+    //         tg.removeClass().addClass('list over');
+    //     }
+    // })
+
 
 })
